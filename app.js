8417974e4b01c9c46a2d5f034d41395a28983c04
John Blossom <jblossom@1up.fm>
2015-08-02 16:57:12 -0400

    inject tabset controller into tabs

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ angular.module('app', [])
     restrict: 'E',
     transclude: true,
     template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
+    require: '^tabset',
     scope: { },
-    link: function(scope, elem, attr) {}
+    link: function(scope, elem, attr, tabsetCtrl) {}
   }
 })
 
