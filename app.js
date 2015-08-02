cc7bc1b6d03aadf58f0077fa59dc5a5b39dac47d
John Blossom <jblossom@1up.fm>
2015-08-02 16:46:02 -0400

    define tabset directive

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 angular.module('app', [])
 
-// Define directives here
+// tab directive
 .directive('tab', function() {
   return {
     restrict: 'E',
@@ -10,6 +10,22 @@ angular.module('app', [])
     template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
     scope: { },
     link: function(scope, elem, attr) {}
+  }
+})
+
+// tabset directive
+.directive('tabset', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: { },
+    templateUrl: 'tabset.html',
+    bindToController: true,
+    controllerAs: 'tabset',
+    controller: function() {
+      var self = this
+      self.tabs = []
+    }
   }
 })
 
